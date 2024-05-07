ca7fecebd371f0c33721b39320711ecb092367fc
ouyanghong <2358560084@qq.com>
2024-05-07 10:20:54 +0800

    药店进销存 项目搭建v0.1(基础版) 加入了一些mavenjar包

--- pharmacy_base_server/src/main/java/com/kgc/service/impl/UnitServiceImpl.java
+++ pharmacy_base_server/src/main/java/com/kgc/service/impl/UnitServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kgc.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.pagehelper.PageHelper;
 import com.kgc.dao.UnitMapper;
 import com.kgc.entity.BaseUnit;
 import com.kgc.service.UnitService;
@@ -10,4 +11,6 @@ import com.kgc.service.UnitService;
  * @create: 2024-05-07 10:02
  **/
 public class UnitServiceImpl extends ServiceImpl<UnitMapper, BaseUnit> implements UnitService {
+    public void k(){
+    }
 }
