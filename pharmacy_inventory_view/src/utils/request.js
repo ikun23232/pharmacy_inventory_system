71d14fad545044b77b1b5786120076b4984f8045
ouyanghong <2358560084@qq.com>
2024-05-07 10:58:49 +0800

    药店进销存 项目搭建v0.1(基础版) 加入了一些mavenjar包 以及一些重放攻击的东西 vue脚手架 配置跨域

--- pharmacy_inventory_view/src/utils/request.js
+++ pharmacy_inventory_view/src/utils/request.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: '/api',
+    baseURL: '/',
     // timeout: 5000
 })
 
 // 添加请求拦截器
 instance.interceptors.request.use(async (request) => {
     // // 添加签名和时间戳
-    // let resp=await axios.get('/api/getUUID')
+    // let resp=await axios.get('/system/getUUID')
     // console.log(resp.data)
     // request.headers['signature'] = resp.data;
     // request.headers['timestamp'] = Date.now();
-
     return request;
 }, (error) => {
     // 处理错误
--- pharmacy_inventory_view/vue.config.js
+++ pharmacy_inventory_view/vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer:{
+    proxy: {
+      '/base': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        ws: false //是否支持websocket
+      },
+      '/menus': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+        ws: false //是否支持websocket
+      },
+      '/visitors': {
+        target: 'http://localhost:8083',
+        changeOrigin: true,
+        ws: false //是否支持websocket
+      }
+    }
+  }
 })
