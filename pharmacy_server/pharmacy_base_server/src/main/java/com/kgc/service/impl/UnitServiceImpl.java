3723b83ab945e82adea0fb994a19559483a5c946
ouyanghong <2358560084@qq.com>
2024-05-08 13:48:57 +0800

    药店进销存 v0.1(基础版) 项目结构大修改(谨慎) 完成计量单位增删查改

--- pharmacy_server/pharmacy_base_server/src/main/java/com/kgc/service/impl/UnitServiceImpl.java
+++ pharmacy_server/pharmacy_base_server/src/main/java/com/kgc/service/impl/UnitServiceImpl.java
@@ -10,9 +10,13 @@ import com.kgc.entity.BaseUnit;
 import com.kgc.entity.Message;
 import com.kgc.entity.Page;
 import com.kgc.service.UnitService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,6 +24,7 @@ import java.util.List;
  * @create: 2024-05-07 10:02
  **/
 @Service
+@Transactional
 public class UnitServiceImpl extends ServiceImpl<UnitMapper, BaseUnit> implements UnitService {
     @Autowired
     private UnitMapper unitMapper;
@@ -50,10 +55,29 @@ public class UnitServiceImpl extends ServiceImpl<UnitMapper, BaseUnit> implement
     @Override
     public Message updateUnit(BaseUnit baseUnit) {
         int updateRow = unitMapper.updateById(baseUnit);
+        if (baseUnit.getUpdatetime()==null){
+            baseUnit.setUpdatetime(new Date());
+            //写死修改人
+            int userId=1;
+            baseUnit.setUpdateby(userId);
+        }
         if (updateRow > 0) {
             return Message.success();
         }
         return Message.error("修改失败");
+    }
+
+    @Override
+    public Message addUnit(BaseUnit baseUnit) {
+        baseUnit.setCreatetime(new Date());
+        //写死修改人
+        int userId=1;
+        baseUnit.setCreateby(userId);
+        int updateRow = unitMapper.insert(baseUnit);
+        if (updateRow > 0) {
+            return Message.success();
+        }
+        return Message.error("添加失败");
     }
 
     @Override
@@ -66,7 +90,12 @@ public class UnitServiceImpl extends ServiceImpl<UnitMapper, BaseUnit> implement
     public Message CheckUnit(String unit, int id) {
         // 查询数据库，看是否存在与新用户名相同的记录，且不是当前要修改的记录
         QueryWrapper<BaseUnit> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("name", unit).ne("id", id); // 查询条件：name 字段等于新用户名，并且 id 不等于当前记录的 id
+        if (id>0){
+            queryWrapper.eq("name", unit).ne("id", id);
+        }else {
+            queryWrapper.eq("name", unit);
+        }
+        // 查询条件：name 字段等于新用户名，并且 id 不等于当前记录的 id
         int updateRow = Math.toIntExact(unitMapper.selectCount(queryWrapper));
         if (updateRow > 0) {
             return Message.error("单位已经存在");
